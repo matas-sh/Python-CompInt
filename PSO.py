b3962d8900fe887bfb9972d55d6f6cb8803dde39
mataxxx5 <m.bartosevicius@gmail.com>
2019-10-18 16:52:45 +0100

    converted bullshit java file

--- PSO.py
+++ PSO.py
@@ -6,12 +6,10 @@ def bounds(nAntennae) :
     dimBnd = [0.0, (nAntennae /2)]
     for i in range(nAntennae) :
         bnds.append(dimBnd)
-    print('bnds : ', bnds)
     return bnds
 
 
 def isValid(design, nAntennae) :
-    print('isValid entered')
     MIN_SPACING = 0.25
     if not len(design) == nAntennae:
         return False
@@ -24,7 +22,6 @@ def isValid(design, nAntennae) :
     for i in range(len(des) - 1) :
         if des[i] < bounds(nAntennae)[i][0] or des[i] > bounds(nAntennae)[i][1] :
             return False
-        print('!!!')
         if des[i+1] - des[i] < MIN_SPACING :
             return False
  
@@ -48,15 +45,12 @@ def powerPeakGen(e, p) :
 
 def evaluate(design, nAntennae, steeringAngle) :
 
-    print('evaluate function entered', not len(design) == nAntennae)
     if not len(design) == nAntennae :
         raise RuntimeError(
             'AntennaArray::evaluate called on design of the wrong size. Expected: ',
             nAntennae, '. Actual: ', len(design)) from error
-    print('first if passed')
     if isValid(design, nAntennae) == False :
         return 1.7976931348623157e+308
-    print('second if passed')    
     peaks = []
     prev = powerPeakGen(0.0, 2.2250738585072014e-308)
     current = powerPeakGen(0.0, arrayFactor(design, 0.0, steeringAngle))
@@ -70,7 +64,6 @@ def evaluate(design, nAntennae, steeringAngle) :
         current = nextOne
         elevation += 0.01
     peaks.append(powerPeakGen(180.0, arrayFactor(design, 180.0, steeringAngle)))
-    print('peaks: ', peaks)
     peaks = sorted(peaks, key = lambda peak: peak["power"])
 
     if len(peaks) < 2 :
